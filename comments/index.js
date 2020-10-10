7ae76b0941de4dc47444c2f347e42d8d06a51ba1
mdee <design4web2014@gmail.com>
2020-10-10 17:23:05 +0330

    adding event but to listen for new events from different servers

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.get('/posts/:postId/comments', (req, res) => {
   res.send(commentsByPostId[req.params.postId] || []);
 });
 
-app.post('/posts/:postId/comments', (req, res) => {
+app.post('/posts/:postId/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
@@ -22,6 +23,15 @@ app.post('/posts/:postId/comments', (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.postId] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.postId
+    }
+  });
 
   res.status(201).send(comments);
 });
