1f7f5e914d80ee70a92fd337a23bc4cde8747a82
mdee <design4web2014@gmail.com>
2020-10-18 15:10:12 +0330

    implementing moderation service in order to moderate a comment

--- comments/index.js
+++ comments/index.js
@@ -20,7 +20,7 @@ app.post('/posts/:postId/comments', async (req, res) => {
 
   const comments = commentsByPostId[req.params.postId] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[req.params.postId] = comments;
 
@@ -29,15 +29,39 @@ app.post('/posts/:postId/comments', async (req, res) => {
     data: {
       id: commentId,
       content,
-      postId: req.params.postId
+      postId: req.params.postId,
+      status: 'pending'
     }
   });
 
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Event Received', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, content, status } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        content,
+        postId,
+        status
+      }
+    });
+  }
 
   res.send({});
 });
